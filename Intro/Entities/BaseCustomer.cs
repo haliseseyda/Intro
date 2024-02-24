2b917d43570435a365d157c36c12cdebab1eeb70
haliseseyda <77547091+haliseseyda@users.noreply.github.com>
2024-02-24 21:21:17 +0300

    Add project files.

---
+++ Intro/Entities/BaseCustomer.cs
@@ -0,0 +1,8 @@
+﻿namespace Intro.Entities;
+
+public class BaseCustomer  // Temel müsteri ortaklar
+{
+    public int Id { get; set; }
+
+    public string CustomerNumber { get; set; }
+}
---
+++ Intro/Entities/Course.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Intro.Entities;
+
+public class Course  //Nesne ve özellikleri ayrıca tipleri  // Veri tutucu sınıflar
+{
+    public int Id { get; set; }
+
+    public string Name { get; set; }
+
+    public string Description { get; set; }
+
+    public double Price { get; set; }
+}
